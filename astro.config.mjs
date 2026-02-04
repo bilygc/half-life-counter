ac0fd3e8effdd80ad75d4ce4bc5f9b6fb6e4a04b
Bily <licbgomez@gmail.com>
2026-02-04 10:13:52 -0700

    change fetching from build time to render time

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://time-life3.com',
-  integrations: [sitemap()]
+  output: 'server',
+  integrations: [sitemap()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
